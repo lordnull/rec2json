8a66f228f2d994f0aeb807a4e9ae6e72ee140593
Micah Warren <micahw@lordnull.com>
2015-07-23 17:16:43 -0500

    Fixed dialyzer warnings.

--- src/rec2json.erl
+++ src/rec2json.erl
@@ -218,7 +218,7 @@ maybe_convertable(_Value, [], _Options) ->
 
 maybe_convertable(Value, [{Mod, Func, Args} | Types], Options) ->
     Arity = length(Args) + 1,
-    code:ensure_loaded(Mod),
+    {module, _} = code:ensure_loaded(Mod),
     case erlang:function_exported(Mod, Func, Arity) of
         true ->
             case erlang:apply(Mod, Func, [Value | Args]) of
